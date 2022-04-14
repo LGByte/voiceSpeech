74ab730d75b7b479d465dd89ef3b9b5b6369bb37
董坤 <dongkun@sdjgroup.com.cn>
2022-04-14 16:48:08 +0800

    44

--- voiceDemo2/AppDelegate.m
+++ voiceDemo2/AppDelegate.m
@@ -72,7 +72,6 @@ didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
 -(void)jpushNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(NSInteger))completionHandler{
     NSDictionary * dict =  notification.request.content.userInfo ;
 //    [self readNote:dict];
-    
     NSLog(@"进入 willPresentNotification %@",notification.request.content.userInfo);
     completionHandler(UNNotificationPresentationOptionAlert | UNNotificationPresentationOptionBadge |UNNotificationPresentationOptionSound );//
 }
